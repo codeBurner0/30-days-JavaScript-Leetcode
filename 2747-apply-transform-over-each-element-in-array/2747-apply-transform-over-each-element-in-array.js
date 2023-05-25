285e8757dd803115774712341329075c84df2531
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-05-25 21:01:41 +0530

    Time: 64 ms (24.41%), Space: 41.7 MB (79.67%) - LeetHub

---
+++ 2747-apply-transform-over-each-element-in-array/2747-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+
+
+// var map = function(arr, fn) {
+//     return arr.map(fn)
+// };
+
+var map = function(arr, fn) {
+    let i=0;
+    while(i<arr.length){
+        arr[i]=fn(arr[i],i);
+        i++;
+    }
+    return arr;
+};
+
+
+
