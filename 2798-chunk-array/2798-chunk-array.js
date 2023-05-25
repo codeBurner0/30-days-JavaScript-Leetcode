02236139f53c3778c5d2c5184a33a324bbce1114
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-05-25 18:31:16 +0530

    Time: 66 ms (46.32%), Space: 45.6 MB (5.84%) - LeetHub

--- 2798-chunk-array/2798-chunk-array.js
+++ 2798-chunk-array/2798-chunk-array.js
@@ -4,11 +4,28 @@
  * @return {Array[]}
  */
 var chunk = function(arr, size) {
-    var res=[];
-    let i=0;
-    while(i<arr.length){
-        res.push(arr.slice(i,i+size));
-        i+=size;
+    // var res=[];
+    // let i=0;
+    // while(i<arr.length){
+    //     res.push(arr.slice(i,i+size));
+    //     i+=size;
+    // }
+    // return res;
+
+    let res=[];
+    let ans=[];
+    if(arr.length==0){
+        return res;
     }
+    arr.map((e)=>{
+        if(ans.length==size){
+            res.push(ans);
+            ans=[];
+            ans.push(e);
+        }else{
+            ans.push(e);
+        }
+    })
+    res.push(ans);
     return res;
 };
