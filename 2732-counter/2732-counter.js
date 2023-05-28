77e43b4a2f79925aefcfe98c00d99043018aa329
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-05-28 20:15:41 +0530

    Time: 65 ms (14.01%), Space: 41.7 MB (76.73%) - LeetHub

--- 2732-counter/2732-counter.js
+++ 2732-counter/2732-counter.js
@@ -13,19 +13,4 @@ var createCounter = function(n) {
  * counter() // 10
  * counter() // 11
  * counter() // 12
-
-
-
-
- class Counter{
-     constructor(n){
-         this.n=n;
-     }
-     increment(){
-         return ++this.n;
-     }
- }
-var add=new Counter(10);
-console.log(add.increment(10));
  */
-
