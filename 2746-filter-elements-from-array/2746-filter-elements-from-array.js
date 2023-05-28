0a5a9fc5f81e959f60e79a0eed2492d1cd97533f
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-05-28 21:39:23 +0530

    Time: 56 ms (69.14%), Space: 41.8 MB (64.44%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -3,16 +3,18 @@
  * @param {Function} fn
  * @return {number[]}
  */
+
+ //var filter=function(arr,fn){
+//     return arr.filter(fn);
+// }
 var filter = function(arr, fn) {
     let res=[];
     let i=0;
     while(i<arr.length){
-        (fn(arr[i]))?res.push(arr[i]):null;
+        if(fn(arr[i],i)){
+            res.push(arr[i]);
+        }
         i++;
     }
-    return arr.filter(fn)
+    return res;
 };
-
-// var filter=function(arr,fn){
-//     return arr.filter(fn);
-// }
