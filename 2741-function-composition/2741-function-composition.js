a74f6960d9aa4677cc96a3f5b25a907c49200901
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-05-29 12:17:52 +0530

    Time: 75 ms (40.68%), Space: 43.4 MB (45.56%) - LeetHub

--- 2741-function-composition/2741-function-composition.js
+++ 2741-function-composition/2741-function-composition.js
@@ -4,16 +4,26 @@
  */
 var compose = function(functions) {
 	return function(x) {
-        let i=functions.length-1;
-        let res=x;
-        while(i>=0){
-            res=functions[i](res);
-            i--;
-        }
-        return res;
+        functions.reduceRight(function(acc,e){
+            acc=e(x);
+            x=acc;
+        },x)
+        return x;
     }
 };
 
+
+// var compose = function(functions) {
+// 	return function(x) {
+//         let i=functions.length-1;
+//         let res=x;
+//         while(i>=0){
+//             res=functions[i](res);
+//             i--;
+//         }
+//         return res;
+//     }
+// };
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
