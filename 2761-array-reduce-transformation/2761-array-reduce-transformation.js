0da2735d9fb970a817af1c80c70b5f65cdd2a320
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-05-29 08:40:37 +0530

    Time: 50 ms (96.19%), Space: 42 MB (88.8%) - LeetHub

--- 2761-array-reduce-transformation/2761-array-reduce-transformation.js
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -5,10 +5,10 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let acc=init;
-    for(let val of nums){
-        acc=fn(acc,val);
-    }
-    return acc;
-    
+    // let acc=init;
+    // for(let val of nums){
+    //     acc=fn(acc,val);
+    // }
+    // return acc;   
+    return nums.reduce(fn,init);
 };
