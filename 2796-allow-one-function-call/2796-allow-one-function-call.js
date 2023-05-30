36b65215551a661da197080d0f6b5bbd4e7ae9e9
Ankit Anand <106039683+codeBurner0@users.noreply.github.com>
2023-05-30 09:51:45 +0530

    Time: 55 ms (76.83%), Space: 42.2 MB (27.04%) - LeetHub

--- 2796-allow-one-function-call/2796-allow-one-function-call.js
+++ 2796-allow-one-function-call/2796-allow-one-function-call.js
@@ -14,17 +14,17 @@
 //     }
 // };
 
-var once = function(fn) {
-    let call=false;
-    return function(...args){
-        if(call){
-            return undefined;
-        }
-        call=true;
-            const a=fn.apply(this,args)
-            return a;  
-    }
-};
+// var once = function(fn) {
+//     let call=false;
+//     return function(...args){
+//         if(call){
+//             return undefined;
+//         }
+//         call=true;
+//             const a=fn.apply(this,args)
+//             return a;  
+//     }
+// };
 
 /**
  * let fn = (a,b,c) => (a + b + c)
@@ -33,3 +33,46 @@ var once = function(fn) {
  * onceFn(1,2,3); // 6
  * onceFn(2,3,6); // returns undefined without calling fn
  */
+
+// var once = function(fn) {
+//     let call=false;
+//     return function(...args){
+//          if(call){
+//              return undefined;
+//          }else{
+//              call=true;
+//              return fn(...args);
+//          }
+//     }
+// };
+var once = function(fn) {
+    let call=false;
+    return function(...args){
+         if(!call){
+             call=true;
+             return fn(...args);
+         }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
